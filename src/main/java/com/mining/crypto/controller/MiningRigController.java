f4da0f23bd1ddb3f8b3e2674478077a8bbfb0bb0
nx <914262748@qq.com>
2025-08-01 07:46:58 +0800

    新增

--- src/main/java/com/mining/crypto/controller/MiningRigController.java
+++ src/main/java/com/mining/crypto/controller/MiningRigController.java
@@ -2,10 +2,7 @@ package com.mining.crypto.controller;
 
 import com.mining.crypto.response.ResponseBean;
 import io.swagger.annotations.ApiImplicitParams;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -24,7 +21,7 @@ public class MiningRigController {
 
     @ApiImplicitParams({@ApiImplicitParam(name = "name", value = "名称", required = true), @ApiImplicitParam(name = "status", value = "状态", required = true)})
     @ApiOperation(value = "修改矿机状态")
-    @GetMapping("/updateStatus")
+    @PostMapping("/updateStatus")
     public ResponseBean<Boolean> updateStatus(@RequestParam(value = "name") String name, @RequestParam(value = "status") String status) {
         return new ResponseBean<>();
     }
